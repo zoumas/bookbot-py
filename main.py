619d766b2b87c0b451ce0a62d26aaa1631d5c84a
zoumas <zoumas@protonmail.com>
2026-04-17 16:20:02 +0300

    2.4: Count Words

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 def main():
     contents = get_book_text("books/frankenstein.txt")
 
-    print(contents)
+    num_words = count_words(contents)
+    print(f"Found {num_words} total words")
+
+
+def count_words(text: str) -> int:
+    return len(text.split())
 
 
 def get_book_text(filepath: str) -> str:
