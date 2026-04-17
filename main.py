c77bc872c915c0fef1963632b110051793b2ec11
zoumas <zoumas@protonmail.com>
2026-04-17 16:06:33 +0300

    2.3: Read File

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+def main():
+    contents = get_book_text("books/frankenstein.txt")
+
+    print(contents)
+
+
+def get_book_text(filepath: str) -> str:
+    with open(filepath) as file:
+        return file.read()
+
+
+main()
